c679d76b31c4131fb004105de7cdd403a35cc964
fortune00 <sinyoungbok99@gmail.com>
2022-07-05 17:09:58 +0900

    [chrore] springboot, hibernate-spatial, postgis 환경 설정

---
+++ src/main/kotlin/com/sia/matcher_kotlin_api/MatcherKotlinApiApplication.kt
@@ -0,0 +1,11 @@
+package com.sia.matcher_kotlin_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MatcherKotlinApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<MatcherKotlinApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/sia/matcher_kotlin_api/MatcherKotlinApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sia.matcher_kotlin_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MatcherKotlinApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
