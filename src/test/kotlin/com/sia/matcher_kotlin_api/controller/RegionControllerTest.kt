edd5181b0a957f8cd7aa8a978d50deee5f0c92e8
fortune00 <sinyoungbok99@gmail.com>
2022-07-06 16:45:11 +0900

    [test] RegionController의 getAOIListInThisRegion 테스트
    - Region의 id가 존재하고 AOIs가 존재하는 경우, 200과 AOI 리스트 반환
    - Region의 id가 존재하고 AOIs가 없는 경우, 200과 빈 리스트 반환
    - Region의 id가 존재하지 않는 경우, 예외 발생

--- src/test/kotlin/com/sia/matcher_kotlin_api/controller/RegionControllerTest.kt
+++ src/test/kotlin/com/sia/matcher_kotlin_api/controller/RegionControllerTest.kt
@@ -1,30 +1,85 @@
 package com.sia.matcher_kotlin_api.controller;
 
-import com.sia.matcher_kotlin_api.AreaForTest
+import com.sia.matcher_kotlin_api.AreaForTest.Companion.aoiReturnDto
+import com.sia.matcher_kotlin_api.AreaForTest.Companion.areaSaveRequest
+import com.sia.matcher_kotlin_api.AreaForTest.Companion.listOfAOIReturnDto
+import com.sia.matcher_kotlin_api.controller.dto.response.AreaListReturnResponse
 import com.sia.matcher_kotlin_api.service.RegionService
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeTypeOf
 import io.mockk.every
 import io.mockk.mockk
 
-class RegionControllerTest: BehaviorSpec({
-    val areaSaveRequest = AreaForTest.areaSaveRequest
+class RegionControllerTest : BehaviorSpec({
 
     val regionService = mockk<RegionService>()
     every {
         regionService.createNewRegion(any())
-    } returns AreaForTest.aoiReturnDto
+    } returns aoiReturnDto
+    every {
+        regionService.hasRegionId(1L)
+    } returns true
+    every {
+        regionService.hasRegionId(2L)
+    } returns true
+    every {
+        regionService.hasRegionId(100L)
+    } returns false
+    every {
+        regionService.readAllAOIInThisRegion(1L)
+    } returns listOfAOIReturnDto
+    every {
+        regionService.readAllAOIInThisRegion(2L)
+    } returns listOf()
 
     val regionController = RegionController(regionService)
 
-    given("postRegion"){
-        `when`("If you request to Save new Region"){
+    given("postRegion") {
+        `when`("If you request to Save new Region") {
             val result = regionController.postRegion(areaSaveRequest)
             then("you can get new Region's id") {
                 result.body!!.id shouldBe 0L
             }
             then("with 200 Status") {
                 result.statusCode.is2xxSuccessful
+            }
+        }
+    }
+
+    given("getAOIListInThisRegion") {
+        `when`("If We have Region 1 and this region contains list of AOI") {
+            val result = regionController.getAOIListInThisRegion("1")
+            then("you will get 3 AOIs") {
+                result.body!!.aois.size shouldBe 3
+            }
+            then("and this type will be a AreaListReturnResponse") {
+                result.body.shouldBeTypeOf<AreaListReturnResponse>()
+            }
+            then("with 200 Status") {
+                result.statusCode.is2xxSuccessful
+            }
+        }
+        `when`("If We have Region 1 but this region contains nothing") {
+            val result = regionController.getAOIListInThisRegion("2")
+            then("you will get a empty List") {
+                result.body!!.aois.isEmpty().shouldBeTrue()
+            }
+            then("but this type will be a AreaListReturnResponse") {
+                result.body.shouldBeTypeOf<AreaListReturnResponse>()
+            }
+            then("with 200 Status") {
+                result.statusCode.is2xxSuccessful
+            }
+        }
+        `when`("If We do not have Region 100") {
+            val exception = shouldThrow<RuntimeException> {
+                regionController.getAOIListInThisRegion("100")
+            }
+            then("Hello Exception!") {
+               exception.message shouldBe "존재하지 않는 ID 입니다"
             }
         }
     }
