813b12c4ac846450933725b827946898abdbcd21
fortune00 <sinyoungbok99@gmail.com>
2022-07-07 14:13:47 +0900

    [test] AOIController의 postAOI 요청 통합 테스트

--- src/test/kotlin/com/sia/matcher_kotlin_api/AreaForTest.kt
+++ src/test/kotlin/com/sia/matcher_kotlin_api/AreaForTest.kt
@@ -9,7 +9,10 @@ import com.sia.matcher_kotlin_api.service.dto.AreaSaveDto
 
 class AreaForTest {
     companion object {
-        val areaSaveRequest = AreaSaveRequest("hello", listOf(Point(0.0, 0.0), Point(0.0, 1.0), Point(1.0, 1.0), Point(1.0, 0.0)))
+        val name = "hello"
+        val points = listOf(Point(0.0, 0.0), Point(0.0, 1.0), Point(1.0, 1.0), Point(1.0, 0.0))
+        val pointsOfMap = listOf(mapOf("x" to 0, "y" to 0), mapOf("x" to 1, "y" to 0), mapOf("x" to 1, "y" to 1), mapOf("x" to 0, "y" to 1))
+        val areaSaveRequest = AreaSaveRequest(name, points)
         val areaSaveDto = AreaSaveDto(areaSaveRequest)
         val aoi = AOI(areaSaveDto)
         val listOfAOI = listOf(aoi, aoi, aoi)
