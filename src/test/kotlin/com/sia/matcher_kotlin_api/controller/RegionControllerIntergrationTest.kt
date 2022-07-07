76a3ae8798d796cb5fcb103dfd647ef341abb08b
fortune00 <sinyoungbok99@gmail.com>
2022-07-07 14:14:06 +0900

    [test] RegionController의 postRegion 요청 통합 테스트

---
+++ src/test/kotlin/com/sia/matcher_kotlin_api/controller/RegionControllerIntergrationTest.kt
@@ -0,0 +1,35 @@
+package com.sia.matcher_kotlin_api.controller
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.sia.matcher_kotlin_api.AreaForTest.Companion.name
+import com.sia.matcher_kotlin_api.AreaForTest.Companion.pointsOfMap
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RegionControllerIntergrationTest @Autowired constructor(val mockMvc: MockMvc) {
+
+    @Test
+    fun postRegionTest() {
+        val uri = "/regions"
+        val body = mapOf("name" to name, "area" to pointsOfMap)
+        val content = jacksonObjectMapper().writeValueAsString(body)
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(uri)
+                .content(content)
+                .contentType(APPLICATION_JSON)
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
+            .andDo(MockMvcResultHandlers.print())
+    }
+}
